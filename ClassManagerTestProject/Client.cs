fc98d59492c848ee7d10519f8479aaad858da53a
Sébastien Muller <sebastienenchine@outlook.com>
2018-05-01 19:14:59 +0800

    .

--- ClassManagerTestProject/Client.cs
+++ ClassManagerTestProject/Client.cs
@@ -1,1 +1,55 @@
-﻿
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Sebastien.ClassManager.Core;
+using Sebastien.ClassManager.Enums;
+
+namespace StudentManagerStudioTestProject
+{
+    [TestClass]
+    public class ClentTest
+    {
+        [TestMethod]
+        public void GetCmdTest1()
+        {
+            //arrange
+            String input = "MyScore";
+            //act
+            Command expected = Command.MyScore;
+            Client.GetCmd(input, out Command cmd);
+            //assert
+            Assert.AreEqual(expected, cmd);
+        }
+        [TestMethod]
+        public void GetCmdTest2()
+        {
+            //arrange
+            String input = "random";
+            //act
+            String expected = null;
+            String actual = Client.GetCmd(input, out Command cmd);
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void IdentityCheckTest()
+        {
+            //arrange
+            Tuple<String, String> info = Tuple.Create("headteacher", "1234");
+            //act
+            User expected = null;
+            User result = Client.IdentityCheck(info);
+            //assert
+            Assert.AreNotEqual(expected, result);
+        }
+        [TestMethod]
+        public void CheckAccountAvailabilityTest()
+        {
+            //arrange
+            String account = "headteacher";
+            //act
+            User result = Client.CheckAccountAvailability(account);
+            //assert
+            Assert.AreNotEqual(result, null);
+        }
+    }
+}

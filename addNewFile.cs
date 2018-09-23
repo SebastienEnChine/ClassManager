30af2f5fa534df5ffee7a1b74d27f6d305cb1ea6
Sebastien Muller <sebastienenchine@outlook.com>
2018-09-24 06:24:56 +0800

    added a file

---
+++ addNewFile.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace SunChaoJie.ClassLibray
+{
+	public class Program
+	{
+		private static void Main(string[] args)
+		{
+			Console.WriteLine("this is a new file.");
+		}
+	}
+}
